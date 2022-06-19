17513247c31dbc9903819d42dedcc7555e0fe8eb
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-06-19 20:34:05 +0300

    for test geometry with OCCT

---
+++ lessons/blok4/geometry_research3/geometry_research3/pch.h
@@ -0,0 +1,8 @@
+// add headers that you want to pre-compile here
+#include <windows.h>
+#include <memory>
+#include <list>
+#include <string>
+//Init all renga header-files
+#import "RengaCOMAPI.tlb"
+#include <Renga/CreateApplication.hpp>
