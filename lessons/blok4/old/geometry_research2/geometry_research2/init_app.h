8cd947d63a04852eec1913f09fc46d7e1fda4ef7
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-14 00:13:27 +0300

    delete all demo-projects

--- lessons/blok4/old/geometry_research2/geometry_research2/init_app.h
+++
@@ -1,8 +0,0 @@
-
-
-//void get_by_grid(Renga::IGridPtr pGrid, TopoDS_Shape* to_add);
-//void visualize_figure(TopoDS_Shape* to_vizualize);
-//void geometry_export_3d(Renga::IProjectPtr project);
-
-
-
