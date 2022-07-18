1f776ad330bac85e4be1e5e2d77f4ad342142099
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-07-18 12:44:25 +0300

    Add project for block 3 -- select last objects

---
+++ lessons/blok3/renga_select_last_objects_2/Event_frame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace renga_select_last_objects_2
+{
+    public partial class Event_frame : Form
+    {
+        public Event_frame()
+        {
+            InitializeComponent();
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].Name = "Время выделения";
+            dataGridView1.Columns[0].Width = 250;
+            dataGridView1.Columns[1].Name = "Кол-во выделенных объектов, шт.";
+            dataGridView1.Columns[1].Width = 200;
+
+            if (init_app.time2selected_objects != null)
+            {
+                foreach (KeyValuePair<string, List<int>> one_record in init_app.time2selected_objects)
+                {
+                    object[] row_data = new object[2] { one_record.Key, one_record.Value.Count };
+                    dataGridView1.Rows.Add(row_data);
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> indesed_selected = new List<int>();
+            Int32 selected_rows_count = dataGridView1.Rows.GetRowCount(DataGridViewElementStates.Selected);
+            if (selected_rows_count > 0)
+            {
+                for (int counter_1 = 0; counter_1 < selected_rows_count; counter_1++)
+                {
+                    indesed_selected.Add(dataGridView1.SelectedRows[counter_1].Index);
+                }
+            }
+            else return;
+            init_app.NeedIncludeSelectionToHistory = false;
+            init_app.ShowSelectedObjects(indesed_selected);
+            init_app.NeedIncludeSelectionToHistory = true;
+            indesed_selected.Clear();
+        }
+    }
+}
---
+++ lessons/blok3/renga_select_last_objects_2/init_app.cs
@@ -0,0 +1,106 @@
+﻿using Renga;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace renga_select_last_objects_2
+{
+    public class init_app : Renga.IPlugin
+    {
+        public static Renga.IApplication renga_app;
+        private Renga.ActionEventSource follow_action;
+        private Renga.SelectionEventSource follow_selection;
+        private Renga.ApplicationEventSource follow_application;
+        public static Dictionary<string, List<int>> time2selected_objects;
+        private string project_path = null;
+        public static bool NeedIncludeSelectionToHistory = true;
+
+
+        public bool Initialize(string pluginFolder)
+        {
+            renga_app = new Renga.Application();
+            follow_application = new ApplicationEventSource(renga_app);
+            follow_application.ProjectClose += (sender, args) =>
+            {
+                time2selected_objects.Clear();
+            };
+            follow_selection = new SelectionEventSource(renga_app.Selection);
+            follow_selection.ModelSelectionChanged += When_user_select;
+
+            Renga.IUI renga_ui = renga_app.UI;
+            Renga.IUIPanelExtension panel = renga_ui.CreateUIPanelExtension();
+
+            Renga.IAction our_button = renga_ui.CreateAction();
+            our_button.ToolTip = "Запустить выбор последних объектов";
+
+            Renga.IImage icon = renga_ui.CreateImage();
+            icon.LoadFromFile(pluginFolder + "\\logo.png");
+            our_button.Icon = icon;
+
+            follow_action = new ActionEventSource(our_button);
+            follow_action.Triggered += (sender, args) =>
+            {
+                Event_frame new_frame = new Event_frame();
+                System.Windows.Forms.Application.Run(new_frame);
+                new_frame.Close();
+            };
+
+            panel.AddToolButton(our_button);
+            renga_ui.AddExtensionToPrimaryPanel(panel);
+
+            return true;
+        }
+        public void Stop()
+        {
+            follow_action.Dispose();
+        }
+        private void When_user_select(object sender, EventArgs args)
+        {
+            if (renga_app.Project == null) return;
+
+            if (this.project_path == null)
+            {
+                this.project_path = renga_app.Project.FilePath;
+                time2selected_objects = new Dictionary<string, List<int>>();
+            }
+            else
+            {
+                if (renga_app.Project.FilePath != project_path)
+                {
+                    time2selected_objects.Clear();
+                    this.project_path = renga_app.Project.FilePath;
+                    time2selected_objects = new Dictionary<string, List<int>>();
+                }
+
+            }
+
+            Renga.ISelection select = renga_app.Selection;
+            if (select.GetSelectedObjects().Length >0)
+            {
+                List<int> current_selection = ((int[])select.GetSelectedObjects()).ToList();
+                string current_date_now = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff");
+                time2selected_objects.Add(current_date_now, current_selection);
+            }
+        }
+        public static void ShowSelectedObjects (List<int> dict_indexes)
+        {
+            List<int> needing_ids = new List<int>();
+            int counter_dict = 0;
+            foreach (KeyValuePair<string, List<int>> one_time2ids in time2selected_objects)
+            {
+                if (dict_indexes.Contains(counter_dict))
+                {
+                    foreach (int one_id in one_time2ids.Value)
+                    {
+                        needing_ids.Add(one_id);
+                    }
+                }
+                counter_dict++;
+            }
+
+            renga_app.Selection.SetSelectedObjects(needing_ids.ToArray());
+            needing_ids.Clear();
+        }
+
+    }
+}
